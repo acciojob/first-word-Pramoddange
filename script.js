3392e8938fa5ff1e9305fb7921696e7750f1d67e
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-05-17 23:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+	if(s.length==0)return'';
+	for(let i=0;i<s.length;i++){
+		if(s[i]==''){
+			return s.substring(0,i);
+		}
+	}
+	
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+//const s = prompt("Enter String:");
+//alert(firstWord(s));
